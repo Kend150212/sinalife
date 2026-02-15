ef38ff5541d56401641d2c013ca3526576981ce8
Ken Dao <kendao@iMac.fios-router.home>
2026-02-14 20:08:54 -0500

    fix: correct CSS import path in order confirmation page

--- src/app/(storefront)/order/[id]/confirmed/page.tsx
+++ src/app/(storefront)/order/[id]/confirmed/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { useParams } from 'next/navigation'
-import '../../checkout/checkout.css'
+import '../../../checkout/checkout.css'
 
 interface OrderData {
     orderNumber: string
